2f3190b0826d1631389eaf001007089b1cc6c636
ekworsham <145511698+ekworsham@users.noreply.github.com>
2025-09-24 21:59:52 -0400

    Implement login process and update routes; add login view with flash messaging

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -11,4 +11,18 @@ async function buildLogin(req, res, next) {
   })
 }
 
-module.exports = { buildLogin }
+/* ****************************************
+*  Process login form submission
+* *************************************** */
+async function loginProcess(req, res, next) {
+  // For now, just re-render the login view with a flash message
+  let nav = await utilities.getNav()
+  req.flash("notice", "Login processing not yet implemented.")
+  res.render("account/login", {
+    title: "Login",
+    nav,
+    messages: req.flash()
+  })
+}
+
+module.exports = { buildLogin, loginProcess }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,7 +4,13 @@ const accountController = require("../controllers/accountController");
 const utilities = require("../utilities");
 
 // GET route for "My Account" page
-router.get("/", utilities.handleErrors(accountController.buildAccount));
+router.get("/", utilities.handleErrors(accountController.buildLogin));
+
+// GET route for login page
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+
+// POST route for login form
+router.post("/login", utilities.handleErrors(accountController.loginProcess));
 
 // Export the router
 module.exports = router;
